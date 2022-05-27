8ea24f43456dcd3371d7efbf93e365e375eed673
Rafli Firdausy Irawan <rafly.firdausy@gmail.com>
2022-05-28 03:50:26 +0700

    REMOVE HEADER

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -50,9 +50,6 @@
                 <li class="nav-item">
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="<?= base_url() ?>" target="_blank" class="nav-link">Halaman Form Input <?= $app_name ?></a>
-                </li>
             </ul>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item dropdown">
